9265bcb78febded1bae0d5ee8b63b0da042d6d39
Lorcheen0814 <lolbinar@gmail.com>
2024-03-08 08:58:36 +0800

    added ioncard and removed explorer in tabl1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -16,7 +16,15 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonCard>
+      <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+      <IonCardHeader>
+        <IonCardTitle>Student</IonCardTitle>
+        <IonCardSubtitle>Lorcheen Olbinar</IonCardSubtitle>
+      </IonCardHeader>
+
+      <IonCardContent> Siomairice</IonCardContent>
+    </IonCard>
       </IonContent>
     </IonPage>
   );
