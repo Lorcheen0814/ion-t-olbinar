a90de7f6429f8c768d6dd3b96674291c0cd77e27
Lorcheen0814 <lolbinar@gmail.com>
2024-03-12 08:23:27 +0800

    added Quick facts button

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,
+   IonAlert, IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -24,7 +25,23 @@ const Tab1: React.FC = () => {
       </IonCardHeader>
 
       <IonCardContent> Siomairice</IonCardContent>
+
+
+
+      <IonButton id="present-alert"expand="full">Quick facts</IonButton>
+    
+    <IonAlert
+      trigger="present-alert"
+      header="A Short Title Is Best"
+      subHeader="A Sub Header Is Optional"
+      message="A message should be a short, complete sentence."
+      buttons={['Action']}
+    ></IonAlert>
+
     </IonCard>
+
+
+
       </IonContent>
     </IonPage>
   );
