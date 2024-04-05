1cd18bb6d48e9d4daee4ac31f1d30d2a25e0b16e
Lorcheen0814 <lolbinar>
2024-04-05 08:14:32 +0800

    added source code of clickcounter

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,8 +1,15 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
+  const [clickCount, setClickCount] = useState(0);
+
+  const incrementClickCount = () => {
+    setClickCount(prevCount => prevCount + 1);
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -17,6 +24,10 @@ const Tab2: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 2 page" />
+        <div>
+          <p>Click Count: {clickCount}</p>
+          <IonButton onClick={incrementClickCount} expand="block">Increment Click Count</IonButton>
+        </div>
       </IonContent>
     </IonPage>
   );
