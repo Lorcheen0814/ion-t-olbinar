96fc7dab646cb933b54629fb2e77e77d83d54362
marychristorrentirasy <marychristorrentirasy@gmail.com>
2024-05-02 19:50:44 +0800

    changed files names and firebase config

--- src/pages/notes/firebase.tsx
+++ src/pages/notes/firebase.tsx
@@ -10,19 +10,15 @@ import { getFirestore } from 'firebase/firestore';
 
 
 // Your web app's Firebase configuration
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAM7B1Lgi5dzn4kL6gEle42eIIaHiUUShc",
-  authDomain: "it35-olbinar.firebaseapp.com",
-  projectId: "it35-olbinar",
-  storageBucket: "it35-olbinar.appspot.com",
-  messagingSenderId: "309194757546",
-  appId: "1:309194757546:web:e4be0db1366024675807d3",
-  measurementId: "G-E1RC4CJFH2"
+  apiKey: "AIzaSyADoz8gG54DJGID5KdUjVaOhs22Flh54mE",
+  authDomain: "ion-t-olbinar.firebaseapp.com",
+  projectId: "ion-t-olbinar",
+  storageBucket: "ion-t-olbinar.appspot.com",
+  messagingSenderId: "396490993297",
+  appId: "1:396490993297:web:b58aa3ef086a4ca7679930",
+  measurementId: "G-S1FH4D1BL9"
 };
 
 
