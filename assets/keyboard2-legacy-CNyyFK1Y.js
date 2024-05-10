c6196545c64109307ae200bcd1964f8038fb57de
Lorcheen0814 <lolbinar@gmail.com>
2024-05-10 09:11:02 +0800

    Updates

--- assets/keyboard2-legacy-CNyyFK1Y.js
+++ assets/keyboard2-legacy-CNyyFK1Y.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DJRNFV59.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-D5SWOOyC.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
